51df14a87a3edd6ae668d2f20204d65a7c97156b
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-03-18 22:07:49 +0300

    TemplateDesignPattern Added

---
+++ TemplateMethodDesignPattern/DesignPattern.TemplateMerhod/TemplatePattern/NetflixPlans.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPattern.TemplateMerhod.TemplatePattern
+{
+    public abstract class NetflixPlans
+    {
+        public void Plan()
+        {
+            PlanType(string.Empty);
+            CountPerson(0);
+            Price(0);
+            ReSolution(string.Empty);
+            Content(string.Empty);
+
+        }
+        public abstract string PlanType(string PlanType);
+        public abstract int CountPerson(int countPerson);
+
+        public abstract double Price(double Price);
+        public abstract string ReSolution(string ReSolution);
+        public abstract decimal Content(string Content);
+
+
+
+
+
+
+
+    }
+}
