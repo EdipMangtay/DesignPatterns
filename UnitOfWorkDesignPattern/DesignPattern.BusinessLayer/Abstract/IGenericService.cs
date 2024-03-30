aaf752c00a0b44a5b10cf137abc2bd3c129b8e7c
Ali Edip Mangtay <mangtay0133@gmail.com>
2024-03-30 23:10:30 +0300

    Abstract and Concrete for BusinessLayer

---
+++ UnitOfWorkDesignPattern/DesignPattern.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void Iinsert(T entity);
+        void Iupdate(T entity);
+        void Idelete(T entity);
+        List<T> IGetAll();
+        T IGetById(int id);
+        void IMultiUpdate(List<T> t);
+        //Bütün burasının amacı CRUD işlemlerini yapabilmek için oluşturulmuş bir interface
+    }
+}
---
+++ UnitOfWorkDesignPattern/DesignPattern.BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,59 @@
+﻿using DesignPattern.BusinessLayer.Abstract;
+using DesignPattern.DataAccesLayer.Abstract;
+using DesignPattern.DataAccesLayer.UnitOfWorks;
+using DesignPattern.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private readonly ICustomerDal _customerDal;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomerManager(ICustomerDal customerDal, IUnitOfWork unitOfWork)
+        {
+            _customerDal = customerDal;
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Idelete(Customer t)
+        {
+            _customerDal.delete(t);
+            _unitOfWork.Save();
+        }
+
+        public List<Customer> IGetAll()
+        {
+            return _customerDal.GetAll();
+        }
+
+        public Customer IGetById(int id)
+        {
+            return _customerDal.GetById(id);
+        }
+
+        public void Iinsert(Customer t)
+        {
+            _customerDal.insert(t);
+            _unitOfWork.Save();
+        }
+
+        public void IMultiUpdate(List<Customer> t)
+        {
+            _customerDal.MultiUpdate(t);
+            _unitOfWork.Save();
+        }
+
+        public void Iupdate(Customer entity)
+        {
+            _customerDal.update(entity);
+            _unitOfWork.Save();
+        }
+    }
+}
